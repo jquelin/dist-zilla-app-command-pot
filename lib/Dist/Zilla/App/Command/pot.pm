6138624a11a74d5cdc848c3a93360ac626198fa7
Jérôme Quelin <jquelin@gmail.com>
2011-02-01 13:57:13 +0100

    initial commit

---
+++ lib/Dist/Zilla/App/Command/pot.pm
@@ -0,0 +1,39 @@
+use 5.012;
+use strict;
+use warnings;
+
+package Dist::Zilla::App::Command::pot;
+# ABSTRACT:
+
+1;
+__END__
+
+=head1 SEE ALSO
+
+You can find more information on this module at:
+
+=over 4
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Dist-Zilla-App-Command-pot>
+
+=item * See open / report bugs
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Dist-Zilla-App-Command-pot>
+
+=item * Git repository
+
+L<http://github.com/jquelin/dist-zilla-app-command-pot>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Dist-Zilla-App-Command-pot>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Dist-Zilla-App-Command-pot>
+
+=back
+
+
